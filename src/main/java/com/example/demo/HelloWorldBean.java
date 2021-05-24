98d8994af8414531d87a746b3db53143363600bf
sjkim1482 <monochrose94@gmail.com>
2021-05-24 14:20:05 +0900

    1. Web Service & Web Application     -Web Service와 Web APplication의 개요
        -Web Service 개발 방법 SOAP과 REST의 이해
    
    2.Spring Boot로 개발하는 RESTful Service
        -Spring Boot 개요
    
        -REST API 설계
    
        -Spring Boot Project 생성
    
        -Spring Boot Project 구조 확인과 실행 방법
    
        -HelloWorld Controller 추가
    
        -HelloWorld Bean 추가
    
        -DispatcherServlet과 프로젝트 동작의 이해
    
        -ath Variable 사용

---
+++ src/main/java/com/example/demo/HelloWorldBean.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+
+
+public class HelloWorldBean {
+	
+	private String msg;
+	
+	public HelloWorldBean(String msg) {
+		this.msg = msg;
+	}
+	
+	public HelloWorldBean() {
+	
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [msg=" + msg + "]";
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	// GET
+	// hello -> (endpoint)
+	// @RequestMapping()
+	@GetMapping(path = "/hello")
+	public String helloWorld() {
+		return "hello";
+	}
+	
+	@GetMapping(path = "/hello-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("hello");
+	}
+	
+	@GetMapping(path = "/hello-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		
+		return new HelloWorldBean(String.format("hello, %s",name));
+	}
+	
+}
